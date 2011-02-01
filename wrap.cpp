f0ac2bdee7d15e2b86c158e6880a067a91ad7c67
syhw <gabriel.synnaeve@gmail.com>
2011-02-01 17:45:18 +0100

    wrap test + fix

--- wrap.cpp
+++ wrap.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string>
+#include <iostream>
+
+#define WRAP_WIDTH 5
 
 using namespace std;
 
@@ -9,22 +12,27 @@ string wrap(const string& input, int cols)
 {
 	if (input.length() < cols)
 		return input;
-	else if (input[cols] == ' ')
+	else if (input[cols - 1] == ' ')
 		return input.substr(0, cols) + string("\n")
-			+ wrap(input.substr(cols+1, input.length() - cols), cols);
+			+ wrap(input.substr(cols, input.length() - cols), cols);
 	else
 	{
 		int ind = input.find_last_of(" ", cols);
 		if (ind == string::npos)
-			ind = cols;
-		return input.substr(0, ind) + string("\n")
-			+ wrap(input.substr(ind+1, input.length() - ind), cols);
+            ind = cols;
+        if (ind != cols)
+            ++ind;
+        return input.substr(0, ind) + string("\n")
+            + wrap(input.substr(ind, input.length() - ind), cols);
 	}
 	return input;
 }
 
 int main(int argc, char* argv[])
 {
-	input = string("lala alal ala lala");
-	printf("input:\n%s,\noutput:\n%s\n", input.c_str(), wrap(input, 5).c_str());
+    while(cin) {
+        getline(cin, input);
+        printf("input:\n%s,\noutput:\n%s\n", input.c_str(), 
+                wrap(input, WRAP_WIDTH).c_str());
+    }
 }
